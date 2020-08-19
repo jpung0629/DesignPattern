8424b66f7c7ffdc5db4519375b550ba3a12590fb
jpung0629 <58873084+jpung0629@users.noreply.github.com>
2020-08-19 12:22:36 +0900

    빌더 패

---
+++ DesignPattern/DesignPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    enum Gender
+    {
+        MAN,
+        WOMAN
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentBuilder builder = new StudentBuilder();
+
+            Student student1 = builder
+                .setAge(12)
+                .setName("풍중현")
+                .setGender(Gender.MAN)
+                .build();
+
+            Student student2 = builder
+                .setAge(21)
+                .setName("국현")
+                .setGender(Gender.MAN)
+                .build();
+
+            Console.WriteLine(student1);
+            Console.WriteLine(student2);
+        }
+    }
+
+    class Student
+    {
+        private string? name;
+        private int? age;
+        private Gender? gender;
+
+        public Student(string? name, int? age, Gender? gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Student: {{name: {name}, age: {age}, gender: {gender}}}";
+        }
+    }
+
+    class StudentBuilder
+    {
+        private string? name = null;
+        private int? age = null;
+        private Gender? gender = null;
+
+        public StudentBuilder()
+        { }
+
+        public StudentBuilder setName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public StudentBuilder setAge(int age)
+        {
+            this.age = age;
+            return this;
+        }
+
+        public StudentBuilder setGender(Gender gender)
+        {
+            this.gender = gender;
+            return this;
+        }
+
+        public Student build()
+        {
+            return new Student(name, age, gender);
+        }
+    }
+}
